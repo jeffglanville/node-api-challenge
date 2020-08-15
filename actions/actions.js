427063eb5b83524288c5bc0147dcc329a8e31c01
Jeff Glanville <jeff.glanville@outlook.com>
2020-08-15 12:37:50 -0500

    add put and delete to actions.js

--- actions/actions.js
+++ actions/actions.js
@@ -46,3 +46,49 @@ router.post("/actions/:id", (req,res) => {
         })
     })
 })
+
+router.put("/actions/:id", (req,res) => {
+    if(!req.body.description || !req.body.notes) {
+        return res.status(400).json({
+            message: "Please provide a description and notes for the action."
+        })
+    }
+
+    actions.update(rea.params.id, req.body)
+    .then((action) => {
+        if (action) {
+            res.status(200).json(action)
+        }else {
+            res.status(400).json({
+                message: "the action with the specified id does not exist."
+            })
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+            message: "The action information could not be modified"
+        })
+    })
+})
+
+router.delete("/actions/:id", (req, res) => {
+    actions.remove(req.params.id)
+    .then((id) => {
+        if (id > 0) {
+            res.status(200).json({
+                message: "The action has been deleted"
+            })
+        }else {
+            return res.status(404).json({
+                message: "The action with the specified id does not exist."
+            })
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json({
+            message: "The action could not be removed"
+        })
+    })
+})
