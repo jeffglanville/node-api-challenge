462d508bb701c92f0c2252bf151cd13539b64d61
Jeff Glanville <jeff.glanville@outlook.com>
2020-08-15 12:42:59 -0500

    update index.js

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const express = require("express")
+
+const actions = require("./actions/actions")
+const projects = require("./projects/projects")
+
+const server = express()
+const port = 3000
+
+server.use(express.json())
+server.use(actions)
+server.use(projects)
+
+server.listen(port, () => {
+    console.log(`Server running at http:localhost:${port}`)
+})
